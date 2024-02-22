a9ffc61b18e02a60cf5206b93f168d281f1dc7c2
alejandronieto-dam <alejandrona5b@gmail.com>
2024-02-22 17:53:20 +0100

    first commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,26 @@
+# CMake generated Testfile for 
+# Source directory: /home/nieto/catkin_ws/src
+# Build directory: /home/nieto/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("turtlebot3_simulations/turtlebot3_simulations")
+subdirs("openai_examples_projects/cartpole_openai_ros_examples")
+subdirs("openai_examples_projects/moving_cube_training")
+subdirs("openai_ros/openai_ros")
+subdirs("openai_examples_projects/fetch_openai_ros_example")
+subdirs("openai_examples_projects/husarion_openai_ros_example")
+subdirs("openai_examples_projects/iriwam_openai_ros_example")
+subdirs("openai_examples_projects/my_hopper_openai_example")
+subdirs("openai_examples_projects/my_moving_cube_pkg")
+subdirs("openai_examples_projects/my_parrotdrone_openai_example")
+subdirs("openai_examples_projects/my_sawyer_openai_example")
+subdirs("openai_examples_projects/my_shadow_tc_openai_example")
+subdirs("openai_examples_projects/my_sumitxl_openai_example")
+subdirs("openai_examples_projects/my_turtlebot3_openai_example")
+subdirs("openai_examples_projects/turtle2_openai_ros_example")
+subdirs("turtlebot3_simulations/turtlebot3_fake")
+subdirs("turtlebot3_simulations/turtlebot3_gazebo")
+subdirs("openai_examples_projects/wamv_deep_qlearning")
+subdirs("openai_examples_projects/wamv_openai_ros_example")
---
+++ build/openai_examples_projects/cartpole_openai_ros_examples/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/nieto/catkin_ws/src/openai_examples_projects/cartpole_openai_ros_examples
+# Build directory: /home/nieto/catkin_ws/build/openai_examples_projects/cartpole_openai_ros_examples
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
