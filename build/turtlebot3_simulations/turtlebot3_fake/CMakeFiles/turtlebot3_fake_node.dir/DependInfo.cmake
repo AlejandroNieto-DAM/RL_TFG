ad67cdb0dcd1235e4eb8dced8ec893d6f80fb67f
alejandronieto-dam <alejandrona5b@gmail.com>
2024-02-26 13:21:52 +0100

    Funciona por fin | Arreglo en environment de turtlebot angular vel estaba en negativo y su comprobacion no

--- build/turtlebot3_simulations/turtlebot3_fake/CMakeFiles/turtlebot3_fake_node.dir/DependInfo.cmake
+++ build/turtlebot3_simulations/turtlebot3_fake/CMakeFiles/turtlebot3_fake_node.dir/DependInfo.cmake
@@ -18,6 +18,7 @@ set(CMAKE_TARGET_DEFINITIONS_CXX
 # The include file search paths:
 set(CMAKE_CXX_TARGET_INCLUDE_PATH
   "/home/nieto/catkin_ws/src/turtlebot3_simulations/turtlebot3_fake/include"
+  "/home/nieto/catkin_ws/devel/include"
   "/opt/ros/noetic/include"
   "/opt/ros/noetic/share/xmlrpcpp/cmake/../../../include/xmlrpcpp"
   )
